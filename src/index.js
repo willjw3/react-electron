97f7a711c972aae42bb077b8ad01e99c5e0e226c
willjw3 <will.j.ward3@gmail.com>
2019-11-06 16:13:36 +0900

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { HashRouter as Router, Route } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Home from "./App"
+import About from "./pages/about"
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <Router>
+        <div>
+            <main>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+            </main>
+        </div>
+    </Router>, 
+    document.getElementById("root")
+    
+)
---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "../App.css"
+
+const About = () => {
+    return (
+        <div className="about">
+            <h1>Welcome to the About Page</h1>
+        </div>
+    )
+}
+
+export default About
